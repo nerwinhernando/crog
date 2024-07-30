b1cd6bec6b229346e49948200b3b25b5c3836395
Nerwin Hernando <nerwinc.hernando@gmail.com>
2024-07-30 21:55:43 +0800

    add sorting by published_at with nulls last

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -3,7 +3,8 @@ class BlogPost < ApplicationRecord
   # validates :body, presence: true
   validates_presence_of :body, on: :create, message: "can't be blank"
 
-  scope :sorted, -> { order(published_at: :desc, updated_at: :desc)}
+  # scope :sorted, -> { order(published_at: :desc, updated_at: :desc)}
+  scope :sorted, -> { order(arel_table[:published_at].desc,null_last).order(updated_at: :desc)}
   scope :draft, -> { where(published_at: nil) }
   scope :published, -> { where("published_at <= ?", Time.current) }
   scope :scheduled, -> { where("published_at >= ?", Time.current) }
